297477a68e102b1a2514f5c688f7664f96959518
Luca Foppiano <luca@foppiano.org>
2019-07-25 10:38:47 +0900

    cleanup

--- postprocess2lines.py
+++ postprocess2lines.py
@@ -20,20 +20,18 @@ import os
 import re
 import sys
 import time
-from os.path import isfile, join, basename, dirname, relpath
-
-import spacy
+from os.path import isfile, join, basename
 
 parser = argparse.ArgumentParser(description='Postprocess sentences. ')
 parser.add_argument('input', metavar='input', type=str, help='Input directory path')
-parser.add_argument('--output', dest='output', help='Output directory path')
+parser.add_argument('output', metavar='output', type=str, help='Output file')
 parser.add_argument('--pattern', dest='input_pattern',
                     help='Pattern for file selection matching the end of the string (xml, java, test.java, etc.)')
 
 args = parser.parse_args()
 
 input_path = args.input
-output_path_root = args.output
+output_path = args.output
 if args.input_pattern is not None:
     input_pattern = args.input_pattern
 else:
@@ -44,59 +42,31 @@ onlyfiles = [os.path.join(dp, f) for dp, dn, fn in os.walk(input_path) for f in 
 
 total_files = len(onlyfiles)
 
-nlp = spacy.load("en_core_web_sm", disable=['ner', 'tagger'])
-
-
-def split_in_sentences(text):
-    doc = nlp(text)
-    sents = []
-    for sent in doc.sents:
-        sents.append(str(sent))
-
-    return sents
-
-
 remove_ref_prefix = [r'Figs?\. ', r'Refs?\. ', r'Secs?\. ', r'Eqs?\. ', r'Tables?\. ', r'\([-– ,]*\)', r'\[[-– ,]*\]',
                      r'\(equation \)']
 
 counter = 0
 start = time.time()
 
-for file in onlyfiles:
-    name = basename(file)
+with open(output_path, 'w') as f_output:
+    for file in onlyfiles:
+        name = basename(file)
 
-    print("Processing " + name)
+        print("Processing " + name)
+        with open(file, 'r') as f:
+            postprocessedLines = []
+            for line in f.readlines():
+                text = line
+                for r in remove_ref_prefix:
+                    text = re.sub(r, '', text)
 
-    with open(file, 'r') as f:
-        postprocessedLines = []
-        for line in f.readlines():
-            text = line
-            for r in remove_ref_prefix:
-                text = re.sub(r, '', text)
+                text = ' '.join(text.split())
+                postprocessedLines.append(text)
 
-            text = ' '.join(text.split())
-            for sentence in split_in_sentences(text):
-                tokens = sentence.split(' ')
-                if len(tokens) > 3:
-                    postprocessedLines.append(sentence)
-
-        if output_path_root is not None:
-            relative_path = relpath(input, input_path)
-
-            output_filename = basename(input).replace('.txt', '.post.txt')
-            output_path = os.path.join(output_path_root, dirname(relative_path))
-            os.makedirs(output_path, exist_ok=True)
-
-            output_file = os.path.join(output_path, output_filename)
-
-            with open(join(output_path, name), 'w') as f_output:
-                for output_line in postprocessedLines:
-                    f_output.write(str(output_line))
-                    f_output.write('\n')
-        else:
-            for output_line in postprocessedLines:
-                print(output_line)
-            print("\n")
+            file_as_line = ' '.join(postprocessedLines)
+            f_output.write(str(file_as_line))
+            f_output.write('\n')
+            f_output.flush()
 
     counter += 1
     if counter % 10 == 0:
